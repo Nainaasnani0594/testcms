0764c8224dd4dec5f475fccf72c9281c6dcd0e37
unknown <nainaasnani0594@gmail.com>
2024-08-07 12:07:46 +0530

    updated

---
+++ resources/js/Components/TaskGraph.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <!-- Task Graph Content -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TaskGraph',
+    // Component logic
+  };
+  </script>
+  
+  <style scoped>
+  /* Component styles */
+  </style>
+  
